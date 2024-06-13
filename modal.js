72c9b06fc8efd4e003a38f1d57945a2caff5ab20
Ryanfs-ng <ryanfsilva6@gmail.com>
2024-06-13 17:24:01 -0300

    Calendario 2

--- modal.js
+++ modal.js
@@ -5,3 +5,16 @@ button.onclick = function() {
     modal.showModal()
 }
 
+const active = document.getElementById("active");
+const unactive = document.getElementById("unactive");
+const container = document.getElementById("container");
+
+active.addEventListener("click", () => {
+    container.classList.add("active");
+})
+
+unactive.addEventListener("click", () => {
+    container.classList.remove("active");
+})
+
+
