6e595316639875241f4817402f10b2f1c40b7d0d
Ryanfs-ng <ryanfsilva6@gmail.com>
2024-06-20 18:26:28 -0300

    Calendário + comentario noticia mudança

---
+++ meses.js
@@ -0,0 +1,17 @@
+let buttonAltera = document.getElementById("prev-month");
+var mes = document.getElementById("month");
+let corCalendario = document.querySelector("teste");
+let buttonAltera2 = document.getElementById("next-month");
+
+buttonAltera.addEventListener('click', (e) => {
+
+    mes.innerHTML = "MAI"
+
+})
+
+buttonAltera2.addEventListener('click', (e) => {
+
+    mes.innerHTML = "JUN"
+  
+
+})
--- modal.js
+++ modal.js
@@ -17,4 +17,3 @@ unactive.addEventListener("click", () => {
     container.classList.remove("active");
 })
 
-
